ad3783bf8046e69796957320899583fc81bda1b6
vishnuvaradhans <vishnuvaradhansoff757@gmail.com>
2026-04-09 21:33:29 +0530

    Coded UC7

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,24 +1,13 @@
 public class HelloApp {
 
     public static void main(String[] args) {
-
-
+        
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
-        }
+        } 
 
-        
-        StringBuilder nameBuilder = new StringBuilder();
-
-
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-    
-        if (nameBuilder.length() > 0) {
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+        else {
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
